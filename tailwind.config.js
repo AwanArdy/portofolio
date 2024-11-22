98559ab6d3cdcdee43f2dcc41bd9c64769200af1
AwanArdy <muhammadawan46@gmail.com>
2024-11-22 21:20:09 +0800

    update app.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,14 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				primary: '#FF4B00',
+				base: '#F8F8F8',
+				textGray: '#D7D9D8',
+				black: '#000000'
+			}
+		}
 	},
 
 	plugins: [typography, forms, containerQueries, aspectRatio]
