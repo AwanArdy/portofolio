e04bb6705c05a2f5d3782aeab1134a5c2d080992
AwanArdy <muhammadawan46@gmail.com>
2024-11-30 23:15:25 +0800

    changes lazyrender

--- src/lib/LazyRender.svelte
+++ src/lib/LazyRender.svelte
@@ -1,40 +1,41 @@
 <script>
-    import { createEventDispatcher, onDestroy, onMount } from "svelte";
+	import { createEventDispatcher, onDestroy, onMount } from 'svelte';
 
-    let isVisible = false;
-    let observer;
-    const dispatch = createEventDispatcher();
+	let isVisible = false;
+	let observer;
+	const dispatch = createEventDispatcher();
 
-    let target;
+	let target;
 
-    onMount(() => {
-        observer = new IntersectionObserver(
-            ([entry]) => {
-                if (entry.isIntersecting) {
-                    isVisible = true;
-                    dispatch("visible");
-                    observer.disconnect();
-                }
-            },
-            { threshold: 0.1 }
-        );
+	onMount(() => {
+		observer = new IntersectionObserver(
+			([entry]) => {
+				if (entry.isIntersecting) {
+					isVisible = true;
+					dispatch('visible');
+					observer.disconnect();
+				}
+			},
+			{ threshold: 0.1 }
+		);
 
-        if (target) observer.observe(target);
-    });
+		if (target) observer.observe(target);
+	});
 
-    onDestroy(() => {
-        observer?.disconnect();
-    });
+	onDestroy(() => {
+		observer?.disconnect();
+	});
 </script>
 
 <div bind:this={target}>
-    {#if isVisible}
-        <slot />
-    {/if}
+	{#if isVisible}
+		<slot />
+	{/if}
 </div>
 
 <style>
-    div {
-        min-height: 100px;
-    }
+	div {
+		min-height: 100px;
+	}
 </style>
+
